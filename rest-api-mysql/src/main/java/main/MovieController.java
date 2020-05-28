8fdf9006c0cd194d58efb0df64d4adc3b27e904c
ChristianMarzianGithub <cmarzian91@gmail.com>
2020-05-28 14:20:47 +0200

    added logging

--- rest-api-mysql/src/main/java/main/MovieController.java
+++ rest-api-mysql/src/main/java/main/MovieController.java
@@ -10,37 +10,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@Controller // This means that this class is a Controller
-@RequestMapping(path="/mettflix") // This means URL's start with /demo (after Application path)
+import org.apache.log4j.Logger;
+
+
+@Controller
+@RequestMapping(path="/mettflix")
 public class MovieController {
 	@Autowired
 	private MovieRepository movieRepository;
+	static Logger log = Logger.getLogger(MovieController.class.getName());
 	
 	@PostMapping(path="/add")
 	public @ResponseBody boolean addNewMovie(@RequestParam String name, @RequestParam String genre, 
 			@RequestParam String bewertung ) {
 		
+		log.info("\"mettflix/add\" aufgerufen");
 		Movie m = new Movie();
 		m.setName(name);
 		m.setGenre(genre);
 		m.setBewertung(bewertung);		
 		movieRepository.save(m);
-		
-		
 		return true;
 	}
 	
 	@DeleteMapping(path=("/delete"))
 	public @ResponseBody boolean deleteMovie(@RequestParam String id) {
-		
-		
-		
+		log.info("\"mettflix/delete\" aufgerufen");
 		movieRepository.deleteById(Integer.valueOf(id));
 		return true;
 	}
 	
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<Movie> getAllMovies(){
+		log.info("\"mettflix/all\" aufgerufen");		
 		return movieRepository.findAll();
 	}
 }
